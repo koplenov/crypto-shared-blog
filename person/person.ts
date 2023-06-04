1208b733c2245aa525f2a705d5603ab6e94000af
koplenov <koplenov@gmail.com>
2023-06-04 14:02:46 +0300

    reduce code

--- person/person.ts
+++ person/person.ts
@@ -7,11 +7,6 @@ namespace $ {
 		}
 
 		@$mol_mem
-		data() {
-			return this.sub( '$blog_posts', $hyoo_crowd_struct )
-		}
-
-		@$mol_mem
 		posts() {
 			const obj = this.sub( '$blog_posts', $blog_entity_links<typeof $blog_post> )
 			obj.Item = $blog_post
